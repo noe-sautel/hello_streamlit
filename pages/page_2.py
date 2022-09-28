f9b5a95b4b0ec8fa3c6684eb7178e985d54e6e5c
Noé <noesautel@icloud.com>
2022-09-28 13:35:29 -0400

    ADD pages

---
+++ pages/page_2.py
@@ -0,0 +1,14 @@
+import imp
+import streamlit as st
+import pandas as pd 
+
+
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
---
+++ pages/page_3.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
+
+st.write("Hello")
